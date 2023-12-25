1ca1b28c6753e9bdef12f051b4d5c3020c88a33e
Rohit Irvisetty <i.rohitroyal@gmail.com>
2023-12-25 01:45:12 -0800

    Adopted React-Query

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,19 @@ import { createRoot } from "react-dom/client";
 import SearchParams from "./SearchParams";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Details from "./Details";
-
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+      cacheTime: Infinity,
+    },
+  },
+});
 const App = () => {
   return (
     <BrowserRouter>
+    <QueryClientProvider client={queryClient}>
     <header>
       <Link>Adopt Me!</Link>
     </header>
@@ -13,6 +22,7 @@ const App = () => {
         <Route path="/details/:id" element={<Details />} />
         <Route path="/" element={<SearchParams />} />
       </Routes>
+      </QueryClientProvider>
     </BrowserRouter>
   );
 };
